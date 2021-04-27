d938d7cab2c8d32c546b8f1d7d12dad2440f844e
Hayk <82840131+Hayk120@users.noreply.github.com>
2021-04-27 21:45:12 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+'''def run(x, y):
+    days = 0
+    while x < y:
+        x = x * 1.1
+        days += 1
+    return days
+
+
+print(run(10, 28)'''
+
+'''year = 2001
+if (year % 4 == 0 and year%100 != 0) or (year%400 == 0):
+    print("nahanj")
+else:
+    print("sovorakan")'''
+
+
+
