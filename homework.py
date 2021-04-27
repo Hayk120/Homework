45a76bc05460563a08f18a0b970f763ca3f2aafc
Hayk <82840131+Hayk120@users.noreply.github.com>
2021-04-27 10:13:10 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,44 @@
+'''def num_of_characters(str):
+    count = 0
+    for each_member in str:
+        if each_member != ' ':
+            count += 1
+
+    return count
+
+
+print(num_of_characters(" I am write a Python program "))'''
+
+'''string = " I go to BDG ";
+count = 0;
+
+for i in range(0, len(string)):
+    if (string[i] != ' '):
+        count = count + 1;
+
+print("Total number of characters in a string: " + str(count));'''
+
+
+'''def factorial(n):
+    if n == 0:
+        return 1
+
+    return n * factorial(n - 1)
+
+num = 5;
+print("Factorial of", num, "is",
+      factorial(num))'''
+
+'''def is_even(number2):
+    even = True
+    if number2 % 2 == 1:
+        even = False
+    return even
+
+print(is_even(4))
+
+def cubes(number3):
+    if is_even(number3) == True:
+        return number3**4
+
+print(cubes(4))'''
